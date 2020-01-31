285d21acfa49f4d9190b07b6504f14410b23de50
Phong Nguyen <58346264+hungphong93@users.noreply.github.com>
2020-01-31 15:28:09 +0100

    adjust input format

--- R/select_gene_sc1.R
+++ R/select_gene_sc1.R
@@ -1,53 +1,35 @@
 library(dplyr)
 
-setwd("/storage/groups/cbm01/workspace/phong.nguyen_new/dream/")
-
-auc = read.csv("aucs.csv", header = T)
-rnaseq = read.csv("rnaseq.csv", header = T, check.names = F)
-
 AnvSigGen = function(rnaseq, auc){
   
-  #rnaseq: dataframe of gene expression data, with genes as rows and samples as columns
-  #auc: dataframe of drug data, with three columns for sample IDs, drug names and AUC values
+  #rnaseq: matrix of gene expression data, with samples as rows and genes as columns
+  #auc: matrix of drug data, samples as rows and drug as columns
   
-  if (!("lab_id" %in% colnames(auc)) ||
-      !("inhibitor" %in% colnames(auc)) ||
-      !("auc" %in% colnames(auc))) {
-    do.call(return, list("Missing columns or incorrect column's names for AUC dataframe, 
-                         Correct colnames: lab_id, inhibitor and auc"))
+  if (rownames(rnaseq) != rownames(auc)) {
+    auc = auc[rownames(rnaseq), ]
   }
   
-  if (!("Gene" %in% colnames(rnaseq))) {
-    do.call(return, list("Missing column 'Gene' or incorrect colname"))
-  }
-  
-  rownames(rnaseq) = rnaseq$Gene
-  
-  if ("Symbol" %in% colnames(rnaseq)) {
-    rnaseq = rnaseq %>% select(-c("Symbol", "Gene"))
-  } else {
-    rnaseq = rnaseq %>% select(-"Gene")
-  }
-  
-  top_expr = rowSums(rnaseq) %>% sort(decreasing = T) %>% head(n= 5000)
-  rnaseq = rnaseq[names(top_expr),]
+  top_expr = colSums(rnaseq) %>% sort(decreasing = T) %>% head(n= 5000)
+  rnaseq = rnaseq[, names(top_expr)]
   
   gen_sig = list()
   
-  for (d in levels(auc$inhibitor)) {
-    auc_d = filter(auc, inhibitor == d) %>% droplevels()
-    if (nrow(auc_d) < 50){
+  for (d in colnames(auc)) {
+    
+    auc_d = auc[,d]  %>% na.omit()
+    
+    if (length(auc_d) < 50){
       next
     }
-    auc_d = auc_d[order(auc_d$auc, decreasing = T),]
-    top_d = head(auc_d$lab_id, n=20) %>% as.character() 
-    bot_d = tail(auc_d$lab_id, n=20) %>% as.character()
+    auc_d = order(auc_d, decreasing = T)
+    top_d = head(auc_d, n=20)  
+    bot_d = tail(auc_d, n=20) 
     p_val_vec = vector(mode = "numeric")
     
-    for (g in rownames(rnaseq)) {
+    for (g in colnames(rnaseq)) {
       rnaseq_g = rnaseq[g,] %>% as.numeric()
-      names(rnaseq_g) = colnames(rnaseq)
-      tmp_df = data.frame(expr = c(rnaseq_g[top_d], rnaseq_g[bot_d]),
+      names(rnaseq_g) = rownames(rnaseq)
+      tmp_df = data.frame(expr = c(rnaseq_g[names(top_d)], rnaseq_g[names(bot_d)]),
                           group = rep(c("top", "bot"), each = 20))
       anv = aov(expr ~ group, tmp_df)
       p_val = summary(anv)[[1]][1,5]
@@ -57,7 +39,7 @@ AnvSigGen = function(rnaseq, auc){
     
     p_val_vec = p.adjust(p_val_vec,"BH")
     index = which(p_val_vec < 0.05)
-    gen_sig_d = rownames(rnaseq)[index]
+    gen_sig_d = colnames(rnaseq)[index]
     
     if (length(gen_sig_d) > 0) {
       gen_sig[[d]] = gen_sig_d
@@ -65,7 +47,7 @@ AnvSigGen = function(rnaseq, auc){
   }
   
   sig = selectGen(gen_sig)
-  rnaseq_sub = rnaseq[sig, ] %>% t() %>% as.data.frame()
+  rnaseq_sub = rnaseq[, sig] 
   
   return(rnaseq_sub)
   
@@ -87,4 +69,4 @@ selectGen = function(gen_sig_list) {
   return(gen_selected)
 }
 
-test = AnvSigGen(rnaseq, auc)
+
