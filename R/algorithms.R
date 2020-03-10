0af2b5fe532194df5e284b8c508be065639f8299
FabioBoniolo <fabio.boniolo@tum.de>
2020-03-10 13:47:45 +0100

    Random Forest for Survival Added

--- R/algorithms.R
+++ R/algorithms.R
@@ -76,6 +76,29 @@ use_cox <- function(
   return(list(pred=pred, diff=diff, fit=fit))
 }######################################################
 
+use_rfSurvival <- function(x_train=x_train,
+y_train=y_train,
+x_test=x_test,
+y_test=y_test,
+hyperparam=hyperparam,
+y_name=y_name,
+seed = F,
+) {
+  Train <- as.data.frame(t(rbind(x_train, y_train)))
+  Pred <-as.data.frame(t(rbind(x_test, y_test)))
+  
+  # 1) train model
+  fit <- rfsrc(Surv(overallSurvival, vitalStatus) ~ .,data = Train, ntree=hyperparam$mtree, mtry =hyperparam$mtry, splitrule ='logrank')
+  # 2) predict on validation set
+  survival.results <- predict.rfsrc(fit, newdata = Pred)
+  # 3) calculate performance on validaiton set
+  Error <- survival.results$err.rate
+  
+  return (list(pred = survival.results, diff = NULL, fit = fit))
+}
+
+
+
 
 use_rf <- function(
   ### Random Forst
